9f978ee96c58c1e7c8a800cbf31f4cca62a93ddb
Svetlana Andreeva <5231328@gmail.com>
2022-10-29 20:48:02 +0300

    fix/внести обязательные исправления

--- scripts/index.js
+++ scripts/index.js
@@ -4,39 +4,25 @@ let popupCloseElement = popupElement.querySelector('.popup__close-button');
 let nameElement = document.querySelector('.profile__name');
 let aboutElement = document.querySelector('.profile__about');
 let inputs = document.querySelectorAll('input');
-
-/* значения полей формы */
-
-inputs[0].value = nameElement.textContent;
-inputs[1].value = aboutElement.textContent;
-
-/* открыть попап */
+let formElement = document.querySelector('.popup__form');
 
 function popupOpen() {
   popupElement.classList.add('popup_opened');
-}
-
-profileEditElement.addEventListener('click', popupOpen);
-
-/* закрыть попап */
-
-function popupClose() {
-  popupElement.classList.remove('popup_opened');
   inputs[0].value = nameElement.textContent;
   inputs[1].value = aboutElement.textContent;
 }
 
-popupCloseElement.addEventListener('click', popupClose);
-
-/* сохранить новое значение полей формы */
-
-let formElement = document.querySelector('.popup__form');
+function popupClose() {
+  popupElement.classList.remove('popup_opened');
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   nameElement.textContent = inputs[0].value;
   aboutElement.textContent = inputs[1].value;
-  popupClose()
+  popupClose();
 }
 
+profileEditElement.addEventListener('click', popupOpen);
+popupCloseElement.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
